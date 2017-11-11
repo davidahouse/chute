de0240ce74c54f8415f657cfac5343e9677e80a9
David House <davidahouse@gmail.com>
2017-11-11 12:05:16 -0500

    Read directly from xccoverage

--- chute/ChuteDetail/ChuteCodeCoverage.swift
+++ chute/ChuteDetail/ChuteCodeCoverage.swift
@@ -12,27 +12,162 @@ struct ChuteCodeCoverage: Encodable {
     let target: String
     let file: String
     let coverage: Double
+    let functions: [ChuteCodeCoverageFunction]
+}
+
+struct ChuteCodeCoverageFunction: Encodable {
+    let name: String
+    let coverage: Double
+}
+
+@objc(IDESchemeActionCodeCoverage) class IDESchemeActionCodeCoverage: NSObject, NSCoding {
+    
+    let codeCoverageTargets: [IDESchemeActionCodeCoverageTarget]
+    
+    required init?(coder aDecoder: NSCoder) {
+        guard let targets = aDecoder.decodeObject(forKey: "buildableCoverageObjects") as? [IDESchemeActionCodeCoverageTarget] else {
+            return nil
+        }
+        codeCoverageTargets = targets
+    }
+    
+    func encode(with aCoder: NSCoder) {
+    }
+}
+
+@objc(IDESchemeActionCodeCoverageTarget) class IDESchemeActionCodeCoverageTarget: NSObject, NSCoding {
+    
+    let productPath: NSString
+    let buildableIdentifier: NSString
+    let sourceFiles: [IDESchemeActionCodeCoverageFile]
+    let lineCoverage: NSNumber
+    let uniqueIdentifier: NSString
+    let name: NSString
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        guard let path = aDecoder.decodeObject(forKey: "productPath") as? NSString,
+                let identifier = aDecoder.decodeObject(forKey: "buildableIdentifier") as? NSString,
+            let files = aDecoder.decodeObject(forKey: "sourceFiles") as? [IDESchemeActionCodeCoverageFile],
+            let coverage = aDecoder.decodeObject(forKey: "lineCoverage") as? NSNumber,
+            let uniqueIdentifier = aDecoder.decodeObject(forKey: "uniqueIdentifier") as? NSString,
+            let name = aDecoder.decodeObject(forKey: "name") as? NSString else {
+                return nil
+        }
+        
+        productPath = path
+        buildableIdentifier = identifier
+        sourceFiles = files
+        lineCoverage = coverage
+        self.uniqueIdentifier = uniqueIdentifier
+        self.name = name
+    }
+    
+    func encode(with aCoder: NSCoder) {
+    }
+}
+
+@objc(IDESchemeActionCodeCoverageFile) class IDESchemeActionCodeCoverageFile: NSObject, NSCoding {
+    
+    let lines: Any?
+    let functions: [IDESchemeActionCodeCoverageFunction]
+    let documentLocation: NSString
+    let lineCoverage: NSNumber
+    let uniqueIdentifier: NSString
+    let name: NSString
+
+    required init?(coder aDecoder: NSCoder) {
+    
+        guard let location = aDecoder.decodeObject(forKey: "documentLocation") as? NSString,
+            let functions = aDecoder.decodeObject(forKey: "functions") as? [IDESchemeActionCodeCoverageFunction],
+            let lines = aDecoder.decodeObject(forKey: "lines"),
+            let coverage = aDecoder.decodeObject(forKey: "lineCoverage") as? NSNumber,
+            let uniqueIdentifier = aDecoder.decodeObject(forKey: "uniqueIdentifier") as? NSString,
+            let name = aDecoder.decodeObject(forKey: "name") as? NSString else {
+                return nil
+        }
+        
+        self.lines = lines
+        self.functions = functions
+        documentLocation = location
+        lineCoverage = coverage
+        self.uniqueIdentifier = uniqueIdentifier
+        self.name = name
+    }
+    
+    func encode(with aCoder: NSCoder) {
+    }
+}
+
+@objc(IDESchemeActionCodeCoverageFunction) class IDESchemeActionCodeCoverageFunction: NSObject, NSCoding {
+    
+    let symbolKind: NSString
+    let lineNumber: Int32
+    let executionCount: Int32
+    let lineCoverage: NSNumber
+    let uniqueIdentifier: NSString
+    let name: NSString
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        guard let symbolKind = aDecoder.decodeObject(forKey: "symbolKindIdentifier") as? NSString,
+            let coverage = aDecoder.decodeObject(forKey: "lineCoverage") as? NSNumber,
+            let uniqueIdentifier = aDecoder.decodeObject(forKey: "uniqueIdentifier") as? NSString,
+            let name = aDecoder.decodeObject(forKey: "name") as? NSString else {
+            return nil
+        }
+
+        self.symbolKind = symbolKind
+        lineNumber = aDecoder.decodeInt32(forKey: "lineNumber")
+        executionCount = aDecoder.decodeInt32(forKey: "executionCount")
+        self.lineCoverage = coverage
+        self.uniqueIdentifier = uniqueIdentifier
+        self.name = name
+    }
+    
+    func encode(with aCoder: NSCoder) { }
+}
+
+@objc(DVTSourceFileLineCoverageData) class DVTSourceFileLineCoverageData: NSObject, NSCoding {
+    required init?(coder aDecoder: NSCoder) {
+    }
+    
+    func encode(with aCoder: NSCoder) { }
 }
 
 extension ChuteCodeCoverage {
-
-    static var codeCoverageURL: URL {
-        return URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponents(["xcov_report", "report.json"])
+    
+    static func codeCoverageURL(testSummaryURL: URL) -> URL {
+        let path = testSummaryURL.path.replacingOccurrences(of: "_TestSummaries.plist", with: ".xccoverage")
+        return URL(fileURLWithPath: path)
     }
 
-    static func findCodeCoverage(path: URL) -> [ChuteCodeCoverage] {
-        // TODO: We should run xcov ourselves here instead of assuming
-        // it was run for us.
-        // xcov --workspace nbc-portal-tv.xcworkspace --scheme nbc-dev-portal-tv --json_report
-        var codeCoverage = [ChuteCodeCoverage]()
-        if let coverage = CodeCoverage.fromFile(file: path) {
+    static func findCodeCoverage(testSummaryURL: URL) -> [ChuteCodeCoverage] {
 
-            for target in coverage.targets {
-                for file in target.files {
-                    codeCoverage.append(ChuteCodeCoverage(target: target.name, file: file.name, coverage: file.coverage))
+        var codeCoverage = [ChuteCodeCoverage]()
+        
+        let path = ChuteCodeCoverage.codeCoverageURL(testSummaryURL: testSummaryURL)
+        guard let rawData = NSKeyedUnarchiver.unarchiveObject(withFile: path.path) as? IDESchemeActionCodeCoverage else {
+            print("--- Error unarchiving xccoverage file \(path)")
+            return codeCoverage
+        }
+        
+        print("--- Code Coverage File: \(path)")
+        
+        for target in rawData.codeCoverageTargets {
+            
+            if !target.name.hasSuffix(".xctest") {
+                for file in target.sourceFiles {
+                    
+                    var functions = [ChuteCodeCoverageFunction]()
+                    for function in file.functions {
+                        functions.append(ChuteCodeCoverageFunction(name: function.name as String, coverage: function.lineCoverage.doubleValue))
+                    }
+                    codeCoverage.append(ChuteCodeCoverage(target: target.name as String, file: file.name as String, coverage: file.lineCoverage.doubleValue, functions: functions))
                 }
             }
         }
+        
         return codeCoverage
     }
 }
